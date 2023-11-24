b746e248b72033fbaaa3118986425caad83741e1
goosedb <berestov.dv@sirius.online>
2023-11-24 11:53:15 +0700

    init commit

---
+++ test/Tests.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Data.Digits
+import Text.Printf
+import Test.QuickCheck
+
+-- | unDigits . digits should be the identity, in any positive base.
+prop_digitsRoundTrip
+    :: Integer -- ^ The integer to test.
+    -> Integer -- ^ The base to use.
+    -> Property
+prop_digitsRoundTrip i b = i > 0 ==> b > 0 ==> i == (unDigits b . digits b) i
+
+
+tests :: [([Char], IO ())]
+tests = [
+    ("checkDigitsRoundTrip", quickCheck prop_digitsRoundTrip)]
+
+main :: IO ()
+main = mapM_ (\(s,a) -> printf "%-25s: " s >> a) tests
+
